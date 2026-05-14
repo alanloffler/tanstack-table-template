e1db42761b3fc0af5bd240aa186586867291beb4
alanloffler <alanmatiasloffler@gmail.com>
2026-05-14 17:25:30 -0300

    feat(main): theme provider

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,14 @@ import { createRoot } from "react-dom/client";
 import "@/index.css";
 import App from "@/App.tsx";
 import { TooltipProvider } from "@/components/ui/tooltip";
+import { ThemeProvider } from "@/providers/theme.provider";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <TooltipProvider>
-      <App />
-    </TooltipProvider>
+    <ThemeProvider defaultTheme="light">
+      <TooltipProvider>
+        <App />
+      </TooltipProvider>
+    </ThemeProvider>
   </StrictMode>,
 );
