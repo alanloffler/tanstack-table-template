d768c881932158f4f5d901c8ad7bc0ef973b6f24
alanloffler <alanmatiasloffler@gmail.com>
2026-05-22 15:53:36 -0300

    feat(components): handle resize column indicator

--- src/components/DraggableColumnHeader.tsx
+++ src/components/DraggableColumnHeader.tsx
@@ -3,26 +3,50 @@ import { TableHead } from "@/components/ui/table";
 import { flexRender, type Header } from "@tanstack/react-table";
 import { useSortable } from "@dnd-kit/sortable";
 
-export function DraggableColumnHeader<TData, TValue>({ header }: { header: Header<TData, TValue> }) {
+export function DraggableColumnHeader<TData, TValue>({
+  header,
+  isLastColumn,
+}: {
+  header: Header<TData, TValue>;
+  isLastColumn?: boolean;
+}) {
   const { attributes, isDragging, listeners, setNodeRef } = useSortable({
     id: header.column.id,
   });
 
   return (
     <TableHead
-      className="cursor-grab active:cursor-grabbing"
+      className="relative overflow-hidden py-2.5"
       ref={setNodeRef}
       style={{
         minWidth: header.column.columnDef.minSize,
-        width: header.column.getSize(),
-        maxWidth: header.column.columnDef.maxSize,
+        width: isLastColumn ? "auto" : `calc(var(--header-${header.id}-size) * 1px)`,
+        maxWidth: isLastColumn ? undefined : header.column.columnDef.maxSize,
         opacity: isDragging ? 0.5 : 1,
-        position: "relative",
       }}
-      {...attributes}
-      {...listeners}
     >
-      {header.isPlaceholder ? null : flexRender(header.column.columnDef.header, header.getContext())}
+      {header.isPlaceholder ? null : (
+        <span className="inline-flex w-fit cursor-grab active:cursor-grabbing" {...attributes} {...listeners}>
+          {flexRender(header.column.columnDef.header, header.getContext())}
+        </span>
+      )}
+      {!isLastColumn && (
+        <div
+          onDoubleClick={() => header.column.resetSize()}
+          onPointerDown={(e) => e.stopPropagation()}
+          onMouseDown={(e) => {
+            e.stopPropagation();
+            header.getResizeHandler()(e);
+          }}
+          onTouchStart={(e) => {
+            e.stopPropagation();
+            header.getResizeHandler()(e);
+          }}
+          className={`hover:bg-primary/50 active:bg-primary absolute top-0 right-0 h-full w-0.75 cursor-col-resize touch-none bg-transparent transition-colors select-none ${
+            header.column.getIsResizing() ? "bg-primary" : ""
+          }`}
+        />
+      )}
     </TableHead>
   );
 }
